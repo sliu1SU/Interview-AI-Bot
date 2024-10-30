70d0cf91ebf9b5d079715c91859c3cb45809f0a7
Sizhe Liu <sliu1@seattleu.edu>
2024-10-30 00:36:44 -0700

    initial commit

---
+++ behavioral_qualifiers.py
@@ -0,0 +1,8 @@
+
+req1 = ("The candidate is willing to take ownership of work within the entire company. The candidate will strive to"
+        "solve a problem whenever he sees it, even if it is not from his own projects.");
+
+req2 = ("The candidate is willing to speak out his mind when he thinks he has a better way to solve a problem, "
+        "even if this action will make other members in the team feel uncomfortable.");
+
+bqReqCollection = [req1, req2];
